d9bb5550d4e17fa9ce2ac2720d8954da96af8b5f
mapaloCat <mapalo2008@gmail.com>
2018-10-22 16:19:29 +0200

    test

--- Shiny App/app.R
+++ Shiny App/app.R
@@ -22,6 +22,7 @@ library(MXM)
 library(corrplot)
 library(corrgram)
 library(ppcor)
+library(tseries) #random library
 
 ###################################################
 ##########           Sources       ################
