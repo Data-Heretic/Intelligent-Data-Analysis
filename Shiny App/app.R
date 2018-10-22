3c23d0100fa7448da0efb52b5e4ea2088d33deff
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2018-10-22 15:09:21 +0200

    Add feature 1

--- Shiny App/app.R
+++ Shiny App/app.R
@@ -22,6 +22,7 @@ library(MXM)
 library(corrplot)
 library(corrgram)
 library(ppcor)
+library(acepack)
 
 ###################################################
 ##########           Sources       ################
