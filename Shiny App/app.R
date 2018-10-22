afafcbce47853135ad23689490857c24e150a6b7
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2018-10-22 15:15:21 +0200

    Comment added

--- Shiny App/app.R
+++ Shiny App/app.R
@@ -22,6 +22,7 @@ library(MXM)
 library(corrplot)
 library(corrgram)
 library(ppcor)
+# Test comment
 
 ###################################################
 ##########           Sources       ################
