2d96436f2f7210e90a38c8d1f341afb764750786
MikeGongolidis <mikegongolidis@hotmail.gr>
2018-10-24 01:25:41 +0300

    Added fancy messages on the top right

--- Shiny App/layout.R
+++ Shiny App/layout.R
@@ -10,6 +10,11 @@ source("00 Hw 1.2/module.R")
 header <- function(id) {
     ns <- NS(id)
     dashboardHeader(title = "Data Analysis",
+                    dropdownMenu(type = "messages",icon = icon("user"),
+                                 messageItem( from = "Data Heretics","Welcome to our shiny dashboard")),
+                    dropdownMenu(type="tasks",badgeStatus = "primary",icon = icon("tasks"),
+                                 taskItem(value = 17, color = "aqua","HW 1.3 coming soon")
+                                 ), 
         tags$li(a(href = 'https://www.fi.upm.es/',
                   img(src = 'Captura.png', title = "a", height = "31px", width = "96px"),
                   style = "padding-top:10px; padding-bottom:10px;"
