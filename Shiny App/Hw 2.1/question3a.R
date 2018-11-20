799c5acebfdbb04aaf5864e77a5e117946b518e9
MikeGongolidis <mikegongolidis@hotmail.gr>
2018-11-20 17:41:03 +0200

    shiny 3a

--- Shiny App/Hw 2.1/question3a.R
+++ Shiny App/Hw 2.1/question3a.R
@@ -8,9 +8,19 @@ hw2.1_q3a_ui <- function(id) {
     ns <- NS(id)
     tabItem(tabName = str_c(id, "Q3a"),
             h2(HTML("<b> Caterage </b>")),
-            h4("Description."),
+            h3("Description."),
             fluidPage(
-
+              h4("After we modified the Carat Size variable and turned it into a categorical one we present the results of the linear model here: "),
+              box(status = "primary",plotOutput(ns("summary3a")))
+            ),
+            fluidPage(
+              h4("The regression model is satisfactory as the overall test of significance for the model is 2.2e-16 and R2 = 0.9953 
+                 (there is an obvious improvement in the model compared to model 1 )"),
+              h4("Afterwards, we started plotting a graph 
+                 using the plot function for the model, as in the below picture."),
+              box(status = "warning",plotOutput(ns("plot.lm3a"))),
+              h4("Interpretation of the Plots:"),
+              box()
             )
     )
 }
@@ -18,5 +28,10 @@ hw2.1_q3a_ui <- function(id) {
 # Server
 
 hw2.1_q3a_server <- function(input, output, session) {
-
+  output$summary3a <- renderPlot({
+    summary(model2)
+  })
+  output$plot.lm3a <- renderPlot({
+    summary(model2)
+  })
 }
