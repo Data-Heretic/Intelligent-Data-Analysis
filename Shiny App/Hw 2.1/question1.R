354eadd2ec3b6728b7956ed9e86481df81a051b4
NewGaramond <ignacio.rodriguez.burgos@gmail.com>
2018-12-18 20:32:42 +0100

    nacho 2

--- Shiny App/Hw 2.1/question1.R
+++ Shiny App/Hw 2.1/question1.R
@@ -15,16 +15,20 @@ hw2.1_q1_ui <- function(id) {
                     main objective for linear regression, As we can see in the below graphs,
                     plotting the log price gives a better representation of the variables."),
              box(status = "warning", plotOutput(ns("plot.lm1"))),
-             box(status = "warning", plotOutput(ns("plot.lm3a")))
+             box(status = "warning", plotOutput(ns("plot.lm2")))
             ),
-        h3("Item nº 2:"),
-        h4("Statistical tests"),
+        h3("The parameters in the given model are represented as below"),
+        
         fluidRow(
-            box(status = "primary",
-                verbatimTextOutput(ns("dwtest3a")),
-                verbatimTextOutput(ns("bptest3a"))),
-            box(status = "primary",
-                verbatimTextOutput(ns("jarque.bera3a")))),
+                 box(status = "primary", verbatimTextOutput(ns("summary3a"))),
+                 p("As we can see in the above summary, the overall model has a Multiple R-squared of 0.9723, so the model is able to predict any Y using X with accuracy of 97%."),
+                 p("Test of significance for the overall model is 2.2e-16, We can reject the null hypothesis which assumes that all the Bi is equal to 1. We can conclude that the variables are significant for the model."),
+                 p("For the weight coefficient: Since the dependent variable is expressed in log, then we can say either: - In log scale: If you increase one unit of cartage , log(price) will increase by 2.855 In Price scale: If you increase one unit of cartage the price of diamond is multiplied by a factor of exp (2.885) =17.37466"),
+                 p("For the color purity coefficient: Since the dependent variable is expressed in log and reference category is “I”, then we can say that the price of “ColourPurityD” is exp(0.41)=1.53 times the price of “ColourPurityI” , we can conclude that “ColourPurityD” is much better than the reference category"),
+                 p(" For the certifiers coefficient, we can see that the HRD and GIA are better than IGI but with a very small difference"),
+                 
+
+            ),
         )
 }
 
