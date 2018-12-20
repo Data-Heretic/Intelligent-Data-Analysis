45d05a84b9b3ed53efabd1daba960797ed61e5a6
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2018-12-20 12:59:53 +0100

    Redesign hw 2.2 question 1 and 2.

--- Shiny App/Hw 2.2/question1.R
+++ Shiny App/Hw 2.2/question1.R
@@ -18,11 +18,18 @@ hw2.2_q1_ui <- function(id) {
                     p("Odds ratio < 1"),
                     verbatimTextOutput(ns("oddsratio.traffic_high")))),
             fluidRow(
-                box(width = 12, plotOutput(ns("plot.podds"))),
-                box(p("In the following fourfold display, we can see a graphical representation for the odds ratio of each traffic level. It includes confidence rings. If they don 't overlap, that indicates an association between crossing and cars, every odds ration is significantly different from 0. In the display we can see a strong positive association for low traffic (5.286842). The rings don’t overlap and the non-principal diagonal sectors have less area than the principal diagonal ones (odds ratio greater than 1). On the other hand the odds of crossing is essentially identical for cars and trucks in case of high traffic (odds ration almost 1)."),
-                    p("This result is an example of Simpson' s paradox. "),
-                    plotOutput(ns("plot.fourfold.partial"))),
-                box(p("From the mosaic plot below We see that there is no systematic association between different actions and the type of vehicle - except among the action of trucks under low traffic. The tiles show that there are relatively less trucks in low traffic with crossing that the hypothesis of independence would predict."),
+                box(width = 12, h3("Interpretations and fourfold display.")),
+                box(plotOutput(ns("plot.podds"))),
+                box(p("The odds ratio is greater than 1 for low traffic which means that the odds of crossing for cars are higher than the odds for trucks."),
+                    p("But in case of high traffic, since the confidence interval contains the value 1, we are going to assume that the odds ratio of crossing for cars is comparable to the odds ratio of crossing for trucks."),
+                    verbatimTextOutput(ns("confint.podds")))),
+            fluidRow(
+                box(p("In the following fourfold display, we can see a graphical representation for the odds ratio of each traffic level. It includes confidence rings. If they don't overlap, that indicates an association between crossing and cars, every odds ration is significantly different from 0. In the display we can see a strong positive association for low traffic (5.286842). The rings don't overlap and the non-principal diagonal sectors have less area than the principal diagonal ones (odds ratio greater than 1). On the other hand the odds of crossing is essentially identical for cars and trucks in case of high traffic (odds ration almost 1)."),
+                    p("This result is an example of Simpson's paradox.")),
+                box(class = "hw2_2_q2_fourfold", plotOutput(ns("plot.fourfold.partial")))),
+            fluidRow(
+                box(width = 12, h3("Partial tables interpretations")),
+                box(width = 12, p("From the mosaic plot below We see that there is no systematic association between different actions and the type of vehicle - except among the action of trucks under low traffic. The tiles show that there are relatively less trucks in low traffic with crossing that the hypothesis of independence would predict."),
                     plotOutput(ns("plot.mosaic.partial"))))))
 }
 
@@ -38,6 +45,11 @@ hw2.2_q1_server <- function(input, output, session) {
     output$oddsratio.traffic_high <- renderPrint({
         req(Elks.partial)
         oddsratio(Elks.partial[,, 2], log = FALSE)
+    })
+
+    output$confint.podds <- renderPrint({
+        req(Elks.podds)
+        confint(Elks.podds, log = FALSE)
     })
 
     output$plot.podds <- renderPlot({
