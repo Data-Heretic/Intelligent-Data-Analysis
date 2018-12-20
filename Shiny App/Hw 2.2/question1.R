432def2fbe5e68a6eaa0f873041f3ad7f416684d
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2018-12-20 11:12:24 +0100

    Design changes in Hw 2.1. Question 1 and 2.

--- Shiny App/Hw 2.2/question1.R
+++ Shiny App/Hw 2.2/question1.R
@@ -16,7 +16,14 @@ hw2.2_q1_ui <- function(id) {
                     verbatimTextOutput(ns("oddsratio.traffic_low"))),
                 box(h4("Table of Action by Vehicle given Traffic, Traffic = High."),
                     p("Odds ratio < 1"),
-                    verbatimTextOutput(ns("oddsratio.traffic_high"))))))
+                    verbatimTextOutput(ns("oddsratio.traffic_high")))),
+            fluidRow(
+                box(width = 12, plotOutput(ns("plot.podds"))),
+                box(p("In the following fourfold display, we can see a graphical representation for the odds ratio of each traffic level. It includes confidence rings. If they don 't overlap, that indicates an association between crossing and cars, every odds ration is significantly different from 0. In the display we can see a strong positive association for low traffic (5.286842). The rings don’t overlap and the non-principal diagonal sectors have less area than the principal diagonal ones (odds ratio greater than 1). On the other hand the odds of crossing is essentially identical for cars and trucks in case of high traffic (odds ration almost 1)."),
+                    p("This result is an example of Simpson' s paradox. "),
+                    plotOutput(ns("plot.fourfold.partial"))),
+                box(p("From the mosaic plot below We see that there is no systematic association between different actions and the type of vehicle - except among the action of trucks under low traffic. The tiles show that there are relatively less trucks in low traffic with crossing that the hypothesis of independence would predict."),
+                    plotOutput(ns("plot.mosaic.partial"))))))
 }
 
 # Server
@@ -31,5 +38,20 @@ hw2.2_q1_server <- function(input, output, session) {
     output$oddsratio.traffic_high <- renderPrint({
         req(Elks.partial)
         oddsratio(Elks.partial[,, 2], log = FALSE)
+    })
+
+    output$plot.podds <- renderPlot({
+        req(Elks.podds)
+        plot(Elks.podds)
+    })
+
+    output$plot.fourfold.partial <- renderPlot({
+        req(Elks.partial)
+        fourfold(Elks.partial)
+    })
+
+    output$plot.mosaic.partial <- renderPlot({
+        req(Elks.partial)
+        mosaic(Elks.partial, shade = T, split_vertical = TRUE)
     })
 }
